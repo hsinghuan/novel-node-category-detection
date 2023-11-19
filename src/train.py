4e7bd688c350c9ee482dee34cb5bb502c028efec
hsinghuan <hhchung@utexas.edu>
2023-11-19 16:34:54 -0600

    add running scripts

---
+++ src/train.py
@@ -0,0 +1,27 @@
+import hydra
+import lightning as L
+from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
+from lightning.pytorch.loggers import TensorBoardLogger
+
+
+def train(config):
+    L.seed_everything(int(config.seed))
+
+
+    # root_dir = os.path.join(config.checkpoint_dir, config.model_name)
+    # os.makedirs(root_dir, exist_ok=True)
+
+    logger: TensorBoardLogger = hydra.utils.instantiate(config.logger, version=config.seed)
+
+    checkpoint_callback = ModelCheckpoint(save_weights_only=True, save_last=True)
+    early_stop_callback = EarlyStopping(monitor="val/loss.discriminator", patience=20, mode="min") # for vanilla pu
+    trainer: L.Trainer = hydra.utils.instantiate(
+        config.trainer,
+        logger=logger,
+        num_sanity_val_steps=0,
+        callbacks=[checkpoint_callback, early_stop_callback]
+    )
+
+    detector: L.LightningModule = hydra.utils.instantiate(config.detector, arch_param=config.arch_param)
+    datamodule: L.LightningDataModule = hydra.utils.instantiate(config.datamodule)
+    trainer.fit(model=detector, datamodule=datamodule)
