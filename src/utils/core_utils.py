5ce77a68dde0d925838af282ab9ece9b4032f4ff
hsinghuan <hhchung@utexas.edu>
2023-12-25 07:52:52 -0600

    add fpr_from_logits

--- src/utils/core_utils.py
+++ src/utils/core_utils.py
@@ -1,7 +1,9 @@
+import numpy as np
 import torch
 import torch.nn as nn
 
 def recall_from_logits(logits, targets, recall_label=1):
+    assert logits.shape[1] == 2 # handle binary classification so far
     indicator_proxy = nn.Softmax(dim=1)
     preds = torch.argmax(logits, dim=1)
     label_counts = torch.bincount(targets.flatten())
@@ -14,3 +16,18 @@ def recall_from_logits(logits, targets, recall_label=1):
 
     recall_proxy = 1 - recall_loss
     return recall, recall_proxy, recall_loss
+
+def fpr_from_logits(logits, targets, positive_label=1):
+    assert logits.shape[1] == 2 # handle binary classification so far
+    indicator_proxy = nn.Softmax(dim=1)
+    preds = torch.argmax(logits, dim=1)
+    label_counts = torch.bincount(targets.flatten())
+    num_labels = logits.shape[1]
+
+    tn = torch.logical_and(preds == 1 - positive_label, targets == 1 - positive_label).sum()
+    fp = torch.logical_and(preds == positive_label, targets == 1 - positive_label).sum()
+    fpr = fp / (fp + tn)
+
+    positive_scores = indicator_proxy(logits)[:, positive_label]
+    fpr_proxy = positive_scores[targets!=positive_label].sum() / (fp + tn)
+    return fpr, fpr_proxy
